25077abf835e48b9cb9a3e52f9e3235388608364
silver-times <rajatsharma3200@gmail.com>
2022-02-28 18:48:41 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="text-center p-10 mt-10 border-t border-gray-200 dark:border-gray-700">
+      <h1>2022 GoogleCLONE, Inc.</h1>
+    </div>
+  );
+};
